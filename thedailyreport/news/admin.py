68b1c7b8fe2058fc34657a86232099ca40ac25f4
antonio-hus <lucian.hus@stud.ubbcluj.ro>
2024-07-12 22:49:12 +0300

    Finished Admin Website

--- thedailyreport/news/admin.py
+++ thedailyreport/news/admin.py
@@ -1,3 +1,20 @@
+# Imports Section
 from django.contrib import admin
+from .models import User, Media, Category, Tag, NewsSource, Article
 
-# Register your models here.
+
+# Display Customization
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title_preview', 'publish_date', 'publisher', 'likes_count', 'favorite_count')
+    search_fields = ('title_preview', 'content_preview', 'writer')
+    list_filter = ('publish_date', 'publisher', 'tags', 'categories')
+    filter_horizontal = ('tags', 'categories', 'medias_full', 'medias_mailing', 'liked_by', 'favorited_by', 'read_later_by')
+
+
+# Model Registration
+admin.site.register(User)
+admin.site.register(Media)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(NewsSource)
+admin.site.register(Article, ArticleAdmin)
